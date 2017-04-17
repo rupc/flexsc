e4a9901dd88579489fe4827b3ca7235f50da441a
yongrae <neurosilver@gmail.com>
2017-04-17 21:48:35 +0900

    add readme for flexsc and flexsc_cpu info

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# FlexSC Implementation  
+Advancedd Operating Systems class from POSTECH, 2017
+
+## Modified files list
+- Makefile -> Add flexsc 
+- flexsc/
+- include/linux/sched.h - struct task_struct -> Add struct syspage
+- arch/x86/entry/syscalls/syscall_64.tbl -> Add system call 400. flexsc_register, 401 flexsc_wait, 402 flexsc_start_hook
+- arch/x86/entry/entry_64.S -> Add system call hooking function
+
+
+---
         Linux kernel release 4.x <http://kernel.org/>
 
 These are the release notes for Linux version 4.  Read them carefully,
--- flexsc/libflexsc/flexsc.h
+++ flexsc/libflexsc/flexsc.h
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include "flexsc_cpu.h"
 
 #define SYSCALL_FLEXSC_REGISTER 400
 #define SYSCALL_FLEXSC_WAIT 401
@@ -16,6 +17,11 @@ struct flexsc_reg_info {
     unsigned long stack_size;
 };
 
+struct flexsc_cpuinfo {
+    unsigned user_cpu;
+    unsigned kernel_cpu;
+};
+
 struct flexsc_sysentry {
     long sysargs[6];
     unsigned nargs;
@@ -24,7 +30,13 @@ struct flexsc_sysentry {
     unsigned sysret;
 } ____cacheline_aligned_in_smp;
 
-void flexsc_register(void);
+struct flexsc_init_info {
+    struct flexsc_sysentry *sysentry; /* Pointer to first sysentry */
+    unsigned npages; /* Number of syspages */
+    struct flexsc_cpuinfo pinned_cpu; 
+};
+
+struct flexsc_sysentry* flexsc_register(void);
 void flexsc_wait(void);
 
 /* Find free sysentry and returns it */
